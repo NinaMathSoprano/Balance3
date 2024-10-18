0829fb67497cc75005800742d8b692c1c1a5a7f3
Nina Bukreeva <Ineya566@gmail.com>
2024-10-18 21:14:30 +0300

    итог,игнор папок

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int S1 = 300;
+        int S2 = 1100;
+        if (S2 >= 1000) System.out.println(S1 + S2 + S2 / 100);
+        else {
+            int S4 = S1 + S2;
+            out.println(S1 + S2);
         }
+
     }
 }
