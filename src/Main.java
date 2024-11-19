25fd85589c8e054ba8bc9219c01bfcd6a0e0a0ad
Nina Bukreeva <Ineya566@gmail.com>
2024-11-19 23:53:57 +0300

    Merge remote-tracking branch 'origin/main'

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
+
 public class Main {
     public static void main(String[] args) {
 
-            int added1 = 300;
+        int added1 = 300;
 
-            int added2 = 1100;
+        int added2 = 1100;
 
-            if (added2 >= 1000) System.out.println(added1 + added2 + added2 / 100);
+        if (added2 >= 1000) System.out.println(added1 + added2 + added2 / 100);
 
-         else {
+        else {
 
-                    int added4 =added1 + added2;
+            int added4 = added1 + added2;
 
-                    System.out.println(added1 + added2);
-                }
-
-
+            System.out.println(added1 + added2);
         }
+
+
     }
+}
